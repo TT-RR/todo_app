fc735a9a1a0bf03663bab48315a2a58f2ac9d050
TT-RR <kaiza1mage@gmail.com>
2023-02-09 11:12:28 +0900

    update

--- source/GoProjects/hello.go
+++ source/GoProjects/hello.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	Watashi_no_Hensu := "GYUDON"       
-	fmt.Println(Watashi_no_Hensu)   
+	Watashi_no_Hensu := "GYUDON"
+	fmt.Println(Watashi_no_Hensu)
+	fmt.Println("Hello", "World")
 }
-
