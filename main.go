c91cab1a57c2654e4966421df0c266f598a7dbd1
Tanaka <118715539+TT-RR@users.noreply.github.com>
2023-03-15 11:07:39 +0900

    Add files via upload

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(config.Config.Port)
+	fmt.Println(config.Config.SQLDriver)
+	fmt.Println(config.Config.DbName)
+	fmt.Println(config.Config.LogFile)
+}
